602bb7d667e7e5b013190044338abbb6b8f10b84
abdulkader99 <abdul.kader.faisal.99@gmail.com>
2026-05-30 09:53:21 +0600

    gallery

--- src/app/pages/RoboSub2026.tsx
+++ src/app/pages/RoboSub2026.tsx
@@ -174,7 +174,7 @@ const RoboSub2026 = () => {
         <div className="absolute inset-0">
           <iframe
             className="absolute top-1/2 left-1/2 w-[300%] h-[300%] -translate-x-1/2 -translate-y-1/2 pointer-events-none"
-            src="https://www.youtube.com/watch?v=Ip7dNVe2cLY"
+            src="https://www.youtube.com/embed/ITj-qZL8_3k?autoplay=1&mute=1&loop=1&playlist=ITj-qZL8_3k&controls=0&showinfo=0&rel=0&modestbranding=1"
             title="RoboSub 2026 Team Video"
             allow="autoplay; encrypted-media"
             frameBorder="0"
